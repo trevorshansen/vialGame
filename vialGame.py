25211302e6fb70e829205d87dcc008a085084330
Trevor Hansen <trevorshansen@gmail.com>
2021-03-23 01:20:03 -0700

    Started working on move method and updated test script

--- vialGame.py
+++ vialGame.py
@@ -1,28 +1,35 @@
 import numpy as np 
 from random import shuffle
 class vialGame:
-# def vialGame(numColors):
 
 	def __init__(self, numColors):
 		self.vials = self.randomGame(numColors)
 
+	# This will create a random new game
 	def randomGame(self, numColors):
 		self.vials =[]
+		# Makes a list of 4 of 1 to numColors and then shuffles
 		options = ((np.array(range(numColors * 4)) + 1) % numColors) + 1
 		shuffle(options)
-		# Creates number of vials + 2
+		# Creates number of vials + 2 empty vials
 		for i in range(0, (numColors * 4),4):
 			self.vials.append(options[i:i+4].tolist())
-			
 		for i in range(2):
-			self.vials.append([0,0,0,0])
+			self.vials.append([])
 
 		return self.vials
 
 	def printGame(self):
+		print("      top<-------->bottom")
 		for i in range(len(self.vials)):
 			print("Vial " + str(i + 1) + ": " + str(self.vials[i]))
 
 	def move(self, fromVial, toVial):
-		return
-	# Need 4 units of each color randomized between numColors # of vials
+		if(len(self.vials[toVial - 1]) == 4):
+			print("That vial is full, please pick a vial with space to move too")
+			return
+		if(len(self.vials[fromVial - 1]) == 0):
+			print("That vial is empty, please pick a filled vial to move from")
+			return
+		# if(self.vials[toVial - 1][0] == self.vials[fromVial - 1][0]):
+		# 	self.vials[toVial - 1].insert(0, self.vials[fromVial - 1].pop(0))
--- vialTest.py
+++ vialTest.py
@@ -2,3 +2,13 @@ from vialGame import vialGame
 
 test = vialGame(5)
 test.printGame()
+
+## Testing move ##
+test.move(1,6)
+test.printGame()
+test.move(1,6)
+test.printGame()
+test.move(1,6)
+test.printGame()
+test.move(1,6)
+test.printGame()
